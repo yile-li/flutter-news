5b5bb3421c4371ad5d690953a5013d774f24b620
yile-li <liyile@umich.edu>
2024-02-15 09:00:40 -0500

    article repository impl start

--- lib/features/daily_news/domain/repository/article_repository.dart
+++ lib/features/daily_news/domain/repository/article_repository.dart
@@ -1,5 +1,5 @@
 import 'package:news_app/core/resources/data_state.dart';
-import 'package:news_app/features/daily_news/domain/entities/article.dart';
+import 'package:news_app/features/daily_news/domain/entities/article_entity.dart';
 
 abstract class ArticleRepository {
   Future<DataState<List<ArticleEntity>>> getNewsArticles();
