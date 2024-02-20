74196f029daa7ba98f8ab34a0153810e8f6cc12b
yile-li <liyile@umich.edu>
2024-02-19 22:21:04 -0500

    update constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '9e6a3e50f635427a98661312a69ad4da';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
